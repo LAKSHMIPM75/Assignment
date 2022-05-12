bfe8196b488355089d7dde34dd86c62172b50e10
LAKSHMIPM75 <101916649+LAKSHMIPM75@users.noreply.github.com>
2022-05-12 22:47:41 +0530

    file added

---
+++ SaveOrUpdate.java
@@ -0,0 +1,29 @@
+package Assigment;
+
+
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class SaveOrUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Session session=UserSession.getSession();
+	    Transaction t=session.beginTransaction(); 
+	    UserDetails userdetails=new UserDetails();
+		userdetails.setPhone("746573624572");
+		userdetails.setUserId(1);
+		userdetails.setUserName("sasirekha");
+		userdetails.setdob(new Date());
+		session.saveOrUpdate(userdetails);
+		userdetails.setUserName("murali");
+		t.commit();
+		System.out.println("updated successfully");
+		session.close();
+		
+	}
+
+}
